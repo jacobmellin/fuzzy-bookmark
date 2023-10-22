777a664f4b7e4903e0602a20bdd5b5aebe34335c
Jacob Mellin <mail@jacobmellin.de>
2023-10-22 12:41:37 +0200

    Wrap extension overlay in shadow root

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 chrome.commands.onCommand.addListener((command) => {
-    console.log(`Command: ${command}`);
+    // console.log(`Command: ${command}`);
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         if(typeof tabs[0] !== 'undefined') {
             chrome.tabs.sendMessage(tabs[0].id!, { command });
--- src/overlay/content.ts
+++ src/overlay/content.ts
@@ -1,21 +1,31 @@
+const container = document.createElement('span');
+const span = document.createElement('span');
 const iframe = document.createElement('iframe');
-const div = document.createElement('div');
 
 iframe.src = chrome.runtime.getURL('src/overlay/index.html');
-//iframe.style = `
-//    position: fixed;
-//    z-index: 10000;
-//    top: 0;
-//    left: 0;
-//    bottom: 0;
-//    right: 0;
-//    width: 100vw;
-//    height: 100vh;
-//`
 
 console.log(iframe.src);
 
-document.documentElement.append(div);
-div.append(iframe);
+document.documentElement.append(container);
+const shadowRoot = container.attachShadow({ mode: 'closed' });
+
+let shadowStyle = document.createElement('style');
+shadowStyle.textContent = `
+    iframe {
+        border: none;
+        width:100vw;
+        height:100vh;
+        position:fixed;
+        top:0;
+        left:0;
+        bottom:0;
+        right:0;
+        pointer-events:none;
+        user-select:none;
+    }
+`
+shadowRoot.append(shadowStyle);
 
 
+shadowRoot.append(span);
+span.append(iframe)
